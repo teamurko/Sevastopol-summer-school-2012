824548ec864f91ca41eead42c85016337c6f5c86
Stanislav Pak <abss.pakso@gmail.com>
2012-07-28 23:53:06 +0400

    Added sample tests, max test, validator [complex]

---
+++ problems/complex/generator_max.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+print(100)
+for i in range(100):
+    print(1000000000 * 1000000000 - 99 + i)
--- problems/complex/validator.cpp
+++ problems/complex/validator.cpp
@@ -1,9 +1,14 @@
 #include "testlib.h"
 
-using namespace std;
-
-int main() {
+int main()
+{
     registerValidation();
-
+    int numTests = inf.readInt(1, 100, "num tests");
+    inf.readEoln();
+    for (int test = 0; test < numTests; ++test) {
+        inf.readLong(1, 1000000000LL * 1000000000LL, "n_i");
+        inf.readEoln();
+    }
+    inf.readEof();
     return 0;
 }
