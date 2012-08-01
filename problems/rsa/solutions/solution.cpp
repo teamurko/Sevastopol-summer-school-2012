20e7a0e461cae01949b9b96a7969766507c6e1e0
Stanislav Pak <abss.pakso@gmail.com>
2012-08-01 23:26:48 +0400

    solution, sample test, validator, wcmp checker for [rsa], wcmp checker for [complex]

--- problems/rsa/solutions/solution.cpp
+++ problems/rsa/solutions/solution.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
+#include <cassert>
+#include <vector>
+typedef long long ll;
 
-int main (int argc, char * const argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+ll gcd(ll a, ll b, ll& x, ll& y)
+{
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    ll xx, yy;
+    ll d = gcd(b % a, a, xx, yy);
+    x = yy - b / a * xx;
+    y = xx;
+    return d;
+}
+
+std::vector<ll> factorize(ll n)
+{
+    std::vector<ll> result;
+    for (int p = 2; p * p <= n; ++p) {
+        while (n % p == 0) {
+            result.push_back(p);
+            n /= p;
+        }
+    }
+    if (n > 1) {
+        result.push_back(n);
+    }
+    return result;
+}
+
+ll pow(ll x, ll n, ll m)
+{
+    ll zweip = x;
+    ll res = 1 % m;
+    while (n > 0) {
+        if (n & 1) res = (res * zweip) % m;
+        zweip *= zweip;
+        zweip %= m;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main (int argc, char * const argv[])
+{
+    int numTests; std::cin >> numTests;
+    for (int test = 0; test < numTests; ++test) {
+        ll e, n, c; std::cin >> e >> n >> c;
+        std::vector<ll> fact = factorize(n);
+        assert(fact.size() == 2);
+        assert(fact.front() != fact.back());
+        ll phi = (fact[0] - 1) * (fact[1] - 1);
+        ll x, y;
+        assert(gcd(e, phi, x, y) == 1);
+        x = (x % phi + phi) % phi;
+        assert((x * e) % phi == 1);
+        std::cout << pow(c, x, n) << std::endl;
+    }
     return 0;
 }
--- problems/rsa/validator.cpp
+++ problems/rsa/validator.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     registerValidation();
-
+    int numTests = inf.readInt(1, 2000, "num tests");
+    inf.readEoln();
+    for (int test = 0; test < numTests; ++test) {
+        //TODO increase limits
+        int e = inf.readInt(1, 32000);
+        inf.readSpace();
+        int n = inf.readInt(1, 32000);
+        inf.readSpace();
+        int c = inf.readInt(1, 32000);
+        inf.readEoln();
+    }
+    inf.readEof();
     return 0;
 }
