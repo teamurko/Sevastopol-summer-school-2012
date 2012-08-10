040cf3ffd8ddf27ccca966ccd9eb0d0ea97f5627
Stanislav Pak <abss.pakso@gmail.com>
2012-08-10 21:35:10 +0400

    standard icmp checker, sample tests, validator [near]

---
+++ problems/near/validator.cpp
@@ -0,0 +1,66 @@
+#include "testlib.h"
+#include <vector>
+#include <queue>
+#include <string>
+using namespace std;
+#define forn(i, n) for (int i = 0; i < int(n); ++i)
+#define forv(i, v) forn(i, v.size())
+
+typedef vector<vector<pair<string, int> > > Graph;
+
+int main()
+{
+    registerValidation();
+    int numVertices = inf.readInt(1, 100000);
+    inf.readSpace();
+    int sortTime = inf.readInt(1, 10);
+    inf.readEoln();
+    Graph graph(numVertices);
+    int root = -1;
+    for (int i = 0; i < numVertices; ++i) {
+        int parent = inf.readInt(-1, numVertices - 1);
+        inf.readSpace();
+        string name = inf.readToken("[a-z]{1,100}", "name");
+        inf.readSpace();
+        inf.readInt(0, 10000);
+        inf.readSpace();
+        inf.readInt(0, 10000);
+        inf.readEoln();
+        if (parent == -1) {
+            ensure(root == -1);
+            root = i;
+        } else {
+            graph[parent].push_back(make_pair(name, i));
+        }
+    }
+    ensure(root != -1);
+    queue<int> q;
+    q.push(root);
+    int reachedCount = 0;
+    while (!q.empty()) {
+        int vertex = q.front();
+        q.pop();
+        ++reachedCount;
+        forv(i, graph[vertex]) {
+            q.push(graph[vertex][i].second);
+        }
+    }
+    ensure(reachedCount == numVertices);
+
+    forv(v, graph) {
+        vector<pair<string, int> >& lst = graph[v];
+        sort(lst.begin(), lst.end());
+        forv(i, lst) {
+            if (i > 0) {
+                ensure(lst[i - 1].first != lst[i].first);
+            }
+        }
+    }
+    inf.readInt(0, numVertices - 1);
+    inf.readSpace();
+    inf.readInt(0, numVertices - 1);
+    inf.readEoln();
+    inf.readEof();
+
+    return 0;
+}
